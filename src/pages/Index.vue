0e10884f6db6a636270a9028ce1ca785ae4ab3b7
MrJSdev <khanriyaz24@gmail.com>
2022-03-01 11:54:42 +0530

    cleaned code coverage folders

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,5 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    {{ message }}
+    Hi, Riyaz 2
   </q-page>
 </template>
-
-<script lang="ts" setup>
-const message = 'Hi, Riyaz 2';
-</script>
